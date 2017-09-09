8ccac65830d614ed82f64cc42133ca42ccd5b26c
krayc <krayc425@gmail.com>
2017-09-09 18:01:22 +0800

    First Commit

---
+++ KCDownloadProgressButton/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  KCDownloadProgressButton
+//
+//  Created by 宋 奎熹 on 2017/9/9.
+//  Copyright © 2017年 宋 奎熹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var kcButton: KCDownloadProgressButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        slider.addTarget(self, action: #selector(valueChanged), for: .valueChanged)
+        
+        /// ❗️该 Button 的 Type 要设为 Custom❗️
+        /// 如：let button = KCDownloadProgressButton(type: .custom)
+        /// 设置按钮初始标题
+        kcButton.setTitle("下载", for: .normal)
+        /// 设置按钮下载时的显示格式
+        kcButton.valueFormatter = {
+            return String(format: "已下载 %d%%", Int($0 * 100))
+        }
+        /// 设置按钮下载完成后的标题
+        kcButton.finishDownloadTitle = "完成"
+        /// 设置按钮边框宽度
+        kcButton.borderWidth = 3.0
+        /// 按钮其它设置
+        kcButton.layer.cornerRadius = 3.0
+        kcButton.layer.masksToBounds = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    @objc func valueChanged() {
+        kcButton.setProgress(slider.value)
+    }
+    
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # KCDownloadProgressButton
+
 A download button with progress in masked text color.
+
+# Preview
+
+![]()
+
+# Usage
+
+Clone this repo and drag `KCDownloadProgressButton.swift` into your project.
+
+## Code
+
+__The type of this button MUST BE Custom__
+
+```
+/// 1. Initialize a button and set other properties
+let kcButton = KCDownloadProgressButton(type: .custom)
+kcButton.frame = CGRect(x: 30, y: 30, width: 100, height: 30)
+kcButton.backgroundColor = .red
+
+/// Set the initial title
+kcButton.setTitle("Download", for: .normal)
+
+/// Set the value formatter
+kcButton.valueFormatter = {
+  return String(format: "Downloaded %d%%", Int($0 * 100))
+}
+
+/// Set the title when download is finished
+kcButton.finishDownloadTitle = "Done"
+
+/// Set the border width
+kcButton.borderWidth = 3.0
+
+self.view.addSubview(kcButton)
+```
+## StoryBoard / xib
+
+1. Drag a `UIButton` into your view, and set its class to `KCDownloadProgressButton`, and set its type to `Custom`.
+2. Set other properties like above.
+
+# TODO
+
+* Enhance efficiency to save memory usage.
+
+# License
+
+__GNU General Public License v3.0__
+
